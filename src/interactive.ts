96d09d1f0eca460fc70822ae326a443abd2303a5
Yoichiro Tanaka <yoichiro6642@gmail.com>
2018-08-20 14:13:22 +0900

    Add test codes.

--- src/interactive.ts
+++ src/interactive.ts
@@ -18,57 +18,73 @@ import * as fs from 'fs'
 import * as readline from 'readline'
 import {ActionsOnGoogle, AssistResponse} from './actions-on-google'
 
-export interface InteractiveParameters {
-    actionName: string | undefined
-    prompt: string | undefined
-    credential: string | undefined
-    locale: string | undefined
-}
-
 export class Interactive {
 
-    constructor(private params: InteractiveParameters) {
+    _action: ActionsOnGoogle
+
+    constructor(
+        private params: {
+            actionName: string | undefined,
+            prompt: string | undefined,
+            credential: string | undefined,
+            locale: string | undefined,
+        }) {
+        const credential = this.params.credential || './credentials.json'
+        const locale = this.params.locale || 'en-US'
+        this._action = this.createActionsOnGoogle(credential, locale)
     }
 
     async start(): Promise<void> {
-        const credential = this.params.credential || './credentials.json'
-        const locale = this.params.locale || 'en-US'
-
-        let action: ActionsOnGoogle | undefined
         try {
-            action = new ActionsOnGoogle(require(fs.realpathSync(credential)))
-            action.setLocale(locale)
-
             const response = await this.invokeAction(
-                action, this.params.actionName, this.params.prompt)
-            await this.conversation(action, response)
+                this.params.actionName, this.params.prompt)
+            await this.conversation(response)
         } catch(e) {
-            if (action) {
-                try {
-                    await action.endConversation()
-                } catch(_) {
-                }
+            try {
+                await this._action.endConversation()
+            } catch(_) {
             }
-            console.log('')
-            console.log(`Conversation aborted: ${e.toString()}`)
+            this.output([
+                '',
+                `Conversation aborted: ${e.toString()}`,
+            ])
         }
     }
 
-    private async conversation(action: ActionsOnGoogle, response: AssistResponse): Promise<void> {
+    output(messages: (string | AssistResponse)[]): void {
+        messages.forEach((message: string) => {
+            console.log(message)
+        })
+    }
+
+    private createActionsOnGoogle(credential: string, locale: string): ActionsOnGoogle {
+        const action = new ActionsOnGoogle(require(fs.realpathSync(credential)))
+        action.setLocale(locale)
+        return action
+    }
+
+    private async conversation(response: AssistResponse): Promise<void> {
         return new Promise<void>(async (resolve, reject) => {
-            console.log('')
-            console.log('Action response:')
-            console.log(response)
-            console.log('')
+            this.output([
+                '',
+                'Action response:',
+                response,
+                '',
+            ])
             try {
                 const isContinue = await this.check(response)
                 if (isContinue) {
-                    const phrase = await this.hear()
-                    const response = await this.send(action, phrase)
-                    await this.conversation(action, response)
+                    let phrase = undefined
+                    while (!phrase || phrase.length === 0) {
+                        phrase = await this.hear()
+                    }
+                    const response = await this.send(phrase)
+                    await this.conversation(response)
                 } else {
-                    await action.endConversation()
-                    console.log('End conversation.')
+                    await this._action.endConversation()
+                    this.output([
+                        'End conversation.',
+                    ])
                 }
                 resolve()
             } catch(e) {
@@ -77,15 +93,14 @@ export class Interactive {
         })
     }
 
-    private async invokeAction(action: ActionsOnGoogle,
-                       actionName: string | undefined,
-                       prompt: string | undefined): Promise<AssistResponse> {
+    private async invokeAction(actionName: string | undefined,
+                               prompt: string | undefined): Promise<AssistResponse> {
         return new Promise<AssistResponse>(async (resolve, reject) => {
             try {
                 const response =
                     actionName ?
-                        await action.startConversationWith(actionName, prompt) :
-                        await action.startConversation(prompt)
+                        await this._action.startConversationWith(actionName, prompt) :
+                        await this._action.startConversation(prompt)
                 resolve(response)
             } catch(e) {
                 reject(e)
@@ -93,8 +108,8 @@ export class Interactive {
         })
     }
 
-    private async hear(): Promise<string> {
-        return new Promise<string>(async (resolve, reject) => {
+    async hear(): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
             const rl = readline.createInterface({
                 input: process.stdin,
                 output: process.stdout,
@@ -104,22 +119,17 @@ export class Interactive {
                 console.log('')
                 reject('SIGINT')
             })
-            rl.question('> ', async answer => {
+            rl.question('> ', answer => {
                 rl.close()
-                if (!answer || answer.length === 0) {
-                    console.log('Please enter a user phrase.')
-                    resolve(await this.hear())
-                } else {
-                    resolve(answer)
-                }
+                resolve(answer)
             })
         })
     }
 
-    private async send(action: ActionsOnGoogle, phrase: string): Promise<AssistResponse> {
+    private async send(phrase: string): Promise<AssistResponse> {
         return new Promise<AssistResponse>(async (resolve, reject) => {
             try {
-                resolve(await action.send(phrase))
+                resolve(await this._action.send(phrase))
             } catch(e) {
                 reject(e)
             }
